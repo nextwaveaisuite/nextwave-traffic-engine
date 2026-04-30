ffceb73e554e8aa8e4d9d0c4ab937713d51cb48c
nextwaveaisuite <nextwaveaisuite@gmail.com>
2026-04-30 19:08:38 +1000

    Initialize Supabase client with URL and key

--- supabase.js
+++ supabase.js
@@ -1,2 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-export const supabase = createClient('YOUR_URL','YOUR_KEY')
+
+const supabaseUrl = 'https://nfqoxklkokrkaabmvzps.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mcW94a2xrb2tya2FhYm12enBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1Mzk4OTMsImV4cCI6MjA5MzExNTg5M30.Y6clpSQdRvsERc_1d3pN-HHCTy0_lO-EmnSZP-0_m3I'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
