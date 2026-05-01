52f263d667642f08395031e6ab00c64ad4d91c80
nextwaveaisuite <nextwaveaisuite@gmail.com>
2026-05-01 20:56:02 +1000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // supabase.js — browser only
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm'
 
-const supabaseUrl = 'https://cvwukmghdmmbchgchtsd.supabase.co'   // ← replace
+const supabaseUrl = 'https://nfqoxklkokrkaabmvzps.supabase.co'   // ← replace
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mcW94a2xrb2tya2FhYm12enBzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1Mzk4OTMsImV4cCI6MjA5MzExNTg5M30.Y6clpSQdRvsERc_1d3pN-HHCTy0_lO-EmnSZP-0_m3I'
 export const supabase = createClient(supabaseUrl, supabaseKey)
